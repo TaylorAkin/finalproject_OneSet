a15afc2e2b0e5c06af71a011fcdf99d21b8fc119
TaylorAkin <taylorakin.ky@gmail.com>
2019-11-26 11:38:40 -0500

    modified Authand Music Tag Controllers

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -11,34 +11,38 @@ use App\Profile;
 use App\Musician;
 use App\Venue;
 use App\MusicianTag;
+use App\Tag;
 
 class AuthenticationController extends Controller
 {
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $user = User::where('email', $request->email)->first();
-        
+
 
         if ($user) {
             if ($request->password == $user->password) {
                 $token = $user->createToken('Laravel Password Grant Client')->accessToken;
                 $profile = Profile::where('user_id', $user->id)->get();
-                $musician = Musician::where('user_id', $user->id )->get();
-                $venue = Venue::where('user_id', $user->id )->get();
-                $tags = [];
-                if(count($musician)>0){
+                $musician = Musician::where('user_id', $user->id)->get();
+                $venue = Venue::where('user_id', $user->id)->get();
+                $musicianTags = [];
+                if (count($musician) > 0) {
 
-                    $tags = MusicianTag::where('musician_id', $musician[0]->user_id )->get();
-                    foreach($tags as $tag){
-                        $name = Tag::where('id', $tag->tag_id)->get();
-                        $tag->name = $name->name;
+                    $musicianTags = MusicianTag::where('musician_id', $musician[0]->user_id)->get();
+                    foreach ($musicianTags as $musicianTag) {
+                        $name = Tag::where('id', $musicianTag->tag_id)->get();
+                        if (count($name) > 0) {
+                            $musicianTag->name = $name;
+                        }
                     }
                 }
-                
-                
+
+
                 $user->musician = $musician;
                 $user->venue = $venue;
                 $user->profile = $profile;
-                $user->tags = $tags;
+                $user->musicianTags = $musicianTags;
                 $response = [
                     'token' => $token,
                     'user' => $user,
@@ -58,39 +62,40 @@ class AuthenticationController extends Controller
     public function logout(Request $request)
     {
         $request->user()->token()->revoke();
-        $request->user()->token()->delete(); 
+        $request->user()->token()->delete();
 
         $response = 'You have been successfully logged out!';
         return response($response, 200);
     }
 
-    public function register(Request $request){
-       
+    public function register(Request $request)
+    {
+
         $this->validate(request(), [
             'name' => 'required',
             'email' => 'required|email',
             'password' => 'required'
         ]);
-        
+
         $user = User::create(request(['name', 'email', 'password']));
         // TODO: 
         // assign the role based on the request value
         //put in model has role table push here? (needs a model?)
 
-        
-        $createprofile = Profile::create(["user_id"=>$user->id, "bio"=>"Edit Me","contact_info" => 'nothing']);
+
+        $createprofile = Profile::create(["user_id" => $user->id, "bio" => "Edit Me", "contact_info" => 'nothing']);
         
         // create musician or venue based on the role
-        if($request->role === 'musician'){
-            
-            $createmusician = Musician::create(["user_id"=>$user->id]);
-        } else{
+        if ($request->role === 'musician') {
 
-            $createvenue = Venue::create(["user_id"=>$user->id]);
+            $createmusician = Musician::create(["user_id" => $user->id]);
+        } else {
+
+            $createvenue = Venue::create(["user_id" => $user->id]);
 
         }
 
-        
+
 
         $token = $user->createToken('Laravel Password Grant Client')->accessToken;
         $user->profile = $createprofile;
